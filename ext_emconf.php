928bfe8abc08d2e84a61dd5c3a141eb69f4c310a
kiefer <kiefer@735d13b6-9817-0410-8766-e36946ffe9aa>
2009-08-06 14:57:29 +0000

    Initial Commit
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/kiwi_popup/trunk@23129 735d13b6-9817-0410-8766-e36946ffe9aa

---
+++ ext_emconf.php
@@ -0,0 +1,46 @@
+<?php
+
+########################################################################
+# Extension Manager/Repository config file for ext: "kiwi_popup"
+#
+# Auto generated 06-08-2009 16:30
+#
+# Manual updates:
+# Only the data in the array - anything else is removed by next write.
+# "version" and "dependencies" must not be touched!
+########################################################################
+
+$EM_CONF[$_EXTKEY] = array(
+	'title' => 'Kiwi Popup',
+	'description' => 'Opens specified image as popup when loading a page (once per session)',
+	'category' => 'fe',
+	'author' => 'Kiwi',
+	'author_email' => 'kiefer-a@web.de',
+	'shy' => '',
+	'dependencies' => '',
+	'conflicts' => '',
+	'priority' => '',
+	'module' => '',
+	'state' => 'alpha',
+	'internal' => '',
+	'uploadfolder' => 1,
+	'createDirs' => '',
+	'modify_tables' => '',
+	'clearCacheOnLoad' => 0,
+	'lockType' => '',
+	'author_company' => '',
+	'version' => '0.0.0',
+	'constraints' => array(
+		'depends' => array(
+		),
+		'conflicts' => array(
+		),
+		'suggests' => array(
+		),
+	),
+	'suggests' => array(
+	),
+	'_md5_values_when_last_written' => 'a:18:{s:9:"ChangeLog";s:4:"3225";s:10:"README.txt";s:4:"ee2d";s:12:"ext_icon.gif";s:4:"1bdc";s:17:"ext_localconf.php";s:4:"8630";s:14:"ext_tables.php";s:4:"368f";s:14:"ext_tables.sql";s:4:"d41d";s:15:"flexform_ds.xml";s:4:"b1d6";s:13:"locallang.xml";s:4:"ea51";s:16:"locallang_db.xml";s:4:"f025";s:19:"doc/wizard_form.dat";s:4:"94f9";s:20:"doc/wizard_form.html";s:4:"682e";s:18:"res/kiwi_popup.css";s:4:"a63c";s:15:"res/lightbox.js";s:4:"96b8";s:14:"pi1/ce_wiz.gif";s:4:"02b6";s:30:"pi1/class.tx_kiwipopup_pi1.php";s:4:"b298";s:38:"pi1/class.tx_kiwipopup_pi1_wizicon.php";s:4:"b29a";s:13:"pi1/clear.gif";s:4:"cc11";s:17:"pi1/locallang.xml";s:4:"9123";}',
+);
+
+?>
---
+++ pi1/class.tx_kiwipopup_pi1.php
@@ -0,0 +1,121 @@
+<?php
+/***************************************************************
+*  Copyright notice
+*
+*  (c) 2009 Kiwi <kiefer-a@web.de>
+*  All rights reserved
+*
+*  This script is part of the TYPO3 project. The TYPO3 project is
+*  free software; you can redistribute it and/or modify
+*  it under the terms of the GNU General Public License as published by
+*  the Free Software Foundation; either version 2 of the License, or
+*  (at your option) any later version.
+*
+*  The GNU General Public License can be found at
+*  http://www.gnu.org/copyleft/gpl.html.
+*
+*  This script is distributed in the hope that it will be useful,
+*  but WITHOUT ANY WARRANTY; without even the implied warranty of
+*  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+*  GNU General Public License for more details.
+*
+*  This copyright notice MUST APPEAR in all copies of the script!
+***************************************************************/
+/**
+ * [CLASS/FUNCTION INDEX of SCRIPT]
+ *
+ * Hint: use extdeveval to insert/update function index above.
+ */
+
+require_once(PATH_tslib.'class.tslib_pibase.php');
+#require_once(PATH_t3lib.'class.t3lib_basicfilefunc.php');
+
+/**
+ * Plugin 'Kiwi Popup' for the 'kiwi_popup' extension.
+ *
+ * @author	Kiwi <kiefer-a@web.de>
+ * @package	TYPO3
+ * @subpackage	tx_kiwipopup
+ */
+class tx_kiwipopup_pi1 extends tslib_pibase {
+	var $prefixId      = 'tx_kiwipopup_pi1';		// Same as class name
+	var $scriptRelPath = 'pi1/class.tx_kiwipopup_pi1.php';	// Path to this script relative to the extension dir.
+	var $extKey        = 'kiwi_popup';	// The extension key.
+	var $pi_checkCHash = true;
+	
+	/**
+	 * The main method of the PlugIn
+	 *
+	 * @param	string		$content: The PlugIn content
+	 * @param	array		$conf: The PlugIn configuration
+	 * @return	The content that is displayed on the website
+	 */
+	function main($content, $conf) {
+		$this->conf = $conf;
+		$this->pi_setPiVarDefaults();
+		$this->pi_loadLL();
+		
+		// GET FLEXFORM DATA
+		$this->pi_initPIflexForm();
+		$piFlexForm = $this->cObj->data['pi_flexform'];
+		if (is_array($piFlexForm['data'])) {
+			foreach ( $piFlexForm['data'] as $sheet => $data ) {
+				foreach ( $data as $lang => $value ) {
+					foreach ( $value as $key => $val ) {
+						$this->ffdata[$key] = $this->pi_getFFvalue($piFlexForm, $key, $sheet);
+					}
+				}
+			}
+		}
+		
+		// get session data 
+		$this->sD = $GLOBALS['TSFE']->fe_user->getKey('ses','tx_kiwipopup');
+		
+		
+		// show only once per session
+		if ($this->sD['shown'] != $GLOBALS['TSFE']->id) {
+		
+			// generate file path
+			$filePath = 'uploads/tx_kiwipopup/'.$this->ffdata['popupfile'];
+			
+			// check if path is valid
+			if (t3lib_div::validPathStr($filePath)) {
+				
+				// loadCSS
+				$cssfile = t3lib_extMgm::siteRelPath($this->extKey).'res/kiwi_popup.css';
+				$GLOBALS['TSFE']->additionalHeaderData[$this->prefixId] .= '<link rel="stylesheet" type="text/css" href="'.$cssfile.'" />';
+				
+				// include js
+				$lightboxPath = t3lib_extMgm::siteRelPath($this->extKey).'res/lightbox.js';
+				$GLOBALS['TSFE']->additionalHeaderData[$this->prefixId] .= '<script type="text/javascript" src="'.$lightboxPath.'"></script>';
+				
+				// generate fake link
+				unset($linkconf);
+				$linkconf['parameter'] = $filePath;
+	 			$fileLink =  $this->cObj->typoLink('text',$linkconf);
+				// print fake link and 
+				$content = '
+					<span style="display:none;"><a href="'.$filePath.'" rel="lightbox" id="kiwi_popup_element">image #1</a></span>
+					<script type="text/javascript">
+						var elem = document.getElementById("kiwi_popup_element");
+						initLightbox();
+						showLightbox(elem);
+					</script>';
+				$sessionVars['shown'] = $GLOBALS['TSFE']->id;
+				
+				$GLOBALS['TSFE']->fe_user->setKey('ses', 'tx_kiwipopup', $sessionVars);
+				$GLOBALS['TSFE']->storeSessionData();
+				
+			}
+			return $this->pi_wrapInBaseClass($content);
+		}
+	}
+}
+
+
+
+if (defined('TYPO3_MODE') && $TYPO3_CONF_VARS[TYPO3_MODE]['XCLASS']['ext/kiwi_popup/pi1/class.tx_kiwipopup_pi1.php'])	{
+	include_once($TYPO3_CONF_VARS[TYPO3_MODE]['XCLASS']['ext/kiwi_popup/pi1/class.tx_kiwipopup_pi1.php']);
+}
+
+?>
