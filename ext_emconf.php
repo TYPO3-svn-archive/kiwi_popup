b34236fd0505eb16a2db8eb6f4deaa2f204aff15
kiefer <kiefer@735d13b6-9817-0410-8766-e36946ffe9aa>
2009-08-12 22:02:26 +0000

    - icon artwork added - ext_emconf.php updated when uploading to TER
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/kiwi_popup/trunk@23336 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_emconf.php
+++ ext_emconf.php
@@ -3,7 +3,7 @@
 ########################################################################
 # Extension Manager/Repository config file for ext: "kiwi_popup"
 #
-# Auto generated 06-08-2009 16:30
+# Auto generated 13-08-2009 00:03
 #
 # Manual updates:
 # Only the data in the array - anything else is removed by next write.
@@ -29,7 +29,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'clearCacheOnLoad' => 0,
 	'lockType' => '',
 	'author_company' => '',
-	'version' => '0.0.0',
+	'version' => '0.0.1',
 	'constraints' => array(
 		'depends' => array(
 		),
@@ -40,7 +40,7 @@ $EM_CONF[$_EXTKEY] = array(
 	),
 	'suggests' => array(
 	),
-	'_md5_values_when_last_written' => 'a:18:{s:9:"ChangeLog";s:4:"3225";s:10:"README.txt";s:4:"ee2d";s:12:"ext_icon.gif";s:4:"1bdc";s:17:"ext_localconf.php";s:4:"8630";s:14:"ext_tables.php";s:4:"368f";s:14:"ext_tables.sql";s:4:"d41d";s:15:"flexform_ds.xml";s:4:"b1d6";s:13:"locallang.xml";s:4:"ea51";s:16:"locallang_db.xml";s:4:"f025";s:19:"doc/wizard_form.dat";s:4:"94f9";s:20:"doc/wizard_form.html";s:4:"682e";s:18:"res/kiwi_popup.css";s:4:"a63c";s:15:"res/lightbox.js";s:4:"96b8";s:14:"pi1/ce_wiz.gif";s:4:"02b6";s:30:"pi1/class.tx_kiwipopup_pi1.php";s:4:"b298";s:38:"pi1/class.tx_kiwipopup_pi1_wizicon.php";s:4:"b29a";s:13:"pi1/clear.gif";s:4:"cc11";s:17:"pi1/locallang.xml";s:4:"9123";}',
+	'_md5_values_when_last_written' => 'a:19:{s:9:"ChangeLog";s:4:"3225";s:10:"README.txt";s:4:"ee2d";s:12:"ext_icon.gif";s:4:"b5f8";s:17:"ext_localconf.php";s:4:"8630";s:14:"ext_tables.php";s:4:"368f";s:14:"ext_tables.sql";s:4:"d41d";s:15:"flexform_ds.xml";s:4:"b1d6";s:13:"locallang.xml";s:4:"ea51";s:16:"locallang_db.xml";s:4:"f025";s:14:"doc/manual.sxw";s:4:"eef8";s:19:"doc/wizard_form.dat";s:4:"94f9";s:20:"doc/wizard_form.html";s:4:"682e";s:18:"res/kiwi_popup.css";s:4:"a63c";s:15:"res/lightbox.js";s:4:"96b8";s:14:"pi1/ce_wiz.gif";s:4:"02b6";s:30:"pi1/class.tx_kiwipopup_pi1.php";s:4:"b298";s:38:"pi1/class.tx_kiwipopup_pi1_wizicon.php";s:4:"b29a";s:13:"pi1/clear.gif";s:4:"cc11";s:17:"pi1/locallang.xml";s:4:"9123";}',
 );
 
 ?>
