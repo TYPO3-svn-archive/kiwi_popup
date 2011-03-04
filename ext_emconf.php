301698bf01d134c351b22c9021074101dd013125
kiefer <kiefer@735d13b6-9817-0410-8766-e36946ffe9aa>
2011-03-04 15:51:39 +0000

    new config option: disable session storage; this means popup is shown at every pageload, not only once a session (TER release v 0.1.0)
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/kiwi_popup/trunk@44639 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_emconf.php
+++ ext_emconf.php
@@ -1,27 +1,27 @@
 <?php
 
 ########################################################################
-# Extension Manager/Repository config file for ext: "kiwi_popup"
+# Extension Manager/Repository config file for ext "kiwi_popup".
 #
-# Auto generated 13-08-2009 00:03
+# Auto generated 04-03-2011 16:51
 #
 # Manual updates:
-# Only the data in the array - anything else is removed by next write.
-# "version" and "dependencies" must not be touched!
+# Only the data in the array - everything else is removed by next
+# writing. "version" and "dependencies" must not be touched!
 ########################################################################
 
 $EM_CONF[$_EXTKEY] = array(
 	'title' => 'Kiwi Popup',
-	'description' => 'Opens specified image as popup when loading a page (once per session)',
+	'description' => 'Opens an image as popup when loading a page (once a session or on every page load)',
 	'category' => 'fe',
-	'author' => 'Kiwi',
+	'author' => 'Andreas Kiefer',
 	'author_email' => 'kiefer-a@web.de',
 	'shy' => '',
 	'dependencies' => '',
 	'conflicts' => '',
 	'priority' => '',
 	'module' => '',
-	'state' => 'alpha',
+	'state' => 'stable',
 	'internal' => '',
 	'uploadfolder' => 1,
 	'createDirs' => '',
@@ -29,7 +29,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'clearCacheOnLoad' => 0,
 	'lockType' => '',
 	'author_company' => '',
-	'version' => '0.0.1',
+	'version' => '0.1.0',
 	'constraints' => array(
 		'depends' => array(
 		),
@@ -40,7 +40,7 @@ $EM_CONF[$_EXTKEY] = array(
 	),
 	'suggests' => array(
 	),
-	'_md5_values_when_last_written' => 'a:19:{s:9:"ChangeLog";s:4:"3225";s:10:"README.txt";s:4:"ee2d";s:12:"ext_icon.gif";s:4:"b5f8";s:17:"ext_localconf.php";s:4:"8630";s:14:"ext_tables.php";s:4:"368f";s:14:"ext_tables.sql";s:4:"d41d";s:15:"flexform_ds.xml";s:4:"b1d6";s:13:"locallang.xml";s:4:"ea51";s:16:"locallang_db.xml";s:4:"f025";s:14:"doc/manual.sxw";s:4:"eef8";s:19:"doc/wizard_form.dat";s:4:"94f9";s:20:"doc/wizard_form.html";s:4:"682e";s:18:"res/kiwi_popup.css";s:4:"a63c";s:15:"res/lightbox.js";s:4:"96b8";s:14:"pi1/ce_wiz.gif";s:4:"02b6";s:30:"pi1/class.tx_kiwipopup_pi1.php";s:4:"b298";s:38:"pi1/class.tx_kiwipopup_pi1_wizicon.php";s:4:"b29a";s:13:"pi1/clear.gif";s:4:"cc11";s:17:"pi1/locallang.xml";s:4:"9123";}',
+	'_md5_values_when_last_written' => 'a:19:{s:9:"ChangeLog";s:4:"3225";s:10:"README.txt";s:4:"ee2d";s:12:"ext_icon.gif";s:4:"b5f8";s:17:"ext_localconf.php";s:4:"8630";s:14:"ext_tables.php";s:4:"368f";s:14:"ext_tables.sql";s:4:"d41d";s:15:"flexform_ds.xml";s:4:"6fe8";s:13:"locallang.xml";s:4:"8b5e";s:16:"locallang_db.xml";s:4:"f025";s:14:"doc/manual.sxw";s:4:"e0b3";s:19:"doc/wizard_form.dat";s:4:"94f9";s:20:"doc/wizard_form.html";s:4:"682e";s:14:"pi1/ce_wiz.gif";s:4:"f39a";s:30:"pi1/class.tx_kiwipopup_pi1.php";s:4:"f5d5";s:38:"pi1/class.tx_kiwipopup_pi1_wizicon.php";s:4:"b29a";s:13:"pi1/clear.gif";s:4:"cc11";s:17:"pi1/locallang.xml";s:4:"9123";s:18:"res/kiwi_popup.css";s:4:"a63c";s:15:"res/lightbox.js";s:4:"96b8";}',
 );
 
 ?>
--- pi1/class.tx_kiwipopup_pi1.php
+++ pi1/class.tx_kiwipopup_pi1.php
@@ -28,7 +28,6 @@
  */
 
 require_once(PATH_tslib.'class.tslib_pibase.php');
-#require_once(PATH_t3lib.'class.t3lib_basicfilefunc.php');
 
 /**
  * Plugin 'Kiwi Popup' for the 'kiwi_popup' extension.
@@ -42,7 +41,7 @@ class tx_kiwipopup_pi1 extends tslib_pibase {
 	var $scriptRelPath = 'pi1/class.tx_kiwipopup_pi1.php';	// Path to this script relative to the extension dir.
 	var $extKey        = 'kiwi_popup';	// The extension key.
 	var $pi_checkCHash = true;
-	
+
 	/**
 	 * The main method of the PlugIn
 	 *
@@ -54,7 +53,7 @@ class tx_kiwipopup_pi1 extends tslib_pibase {
 		$this->conf = $conf;
 		$this->pi_setPiVarDefaults();
 		$this->pi_loadLL();
-		
+
 		// GET FLEXFORM DATA
 		$this->pi_initPIflexForm();
 		$piFlexForm = $this->cObj->data['pi_flexform'];
@@ -67,33 +66,35 @@ class tx_kiwipopup_pi1 extends tslib_pibase {
 				}
 			}
 		}
-		
-		// get session data 
-		$this->sD = $GLOBALS['TSFE']->fe_user->getKey('ses','tx_kiwipopup');
-		
-		
-		// show only once per session
-		if ($this->sD['shown'] != $GLOBALS['TSFE']->id) {
-		
+
+		if ($this->ffdata['sessionStorage']) {
+			// get session data
+			$this->sD = $GLOBALS['TSFE']->fe_user->getKey('ses','tx_kiwipopup');
+		}
+
+
+		// show only once per session (if activated in ff)
+		if (!$this->ffdata['sessionStorage'] || $this->sD['shown'] != $GLOBALS['TSFE']->id) {
+
 			// generate file path
 			$filePath = 'uploads/tx_kiwipopup/'.$this->ffdata['popupfile'];
-			
+
 			// check if path is valid
 			if (t3lib_div::validPathStr($filePath)) {
-				
+
 				// loadCSS
 				$cssfile = t3lib_extMgm::siteRelPath($this->extKey).'res/kiwi_popup.css';
 				$GLOBALS['TSFE']->additionalHeaderData[$this->prefixId] .= '<link rel="stylesheet" type="text/css" href="'.$cssfile.'" />';
-				
+
 				// include js
 				$lightboxPath = t3lib_extMgm::siteRelPath($this->extKey).'res/lightbox.js';
 				$GLOBALS['TSFE']->additionalHeaderData[$this->prefixId] .= '<script type="text/javascript" src="'.$lightboxPath.'"></script>';
-				
+
 				// generate fake link
 				unset($linkconf);
 				$linkconf['parameter'] = $filePath;
 	 			$fileLink =  $this->cObj->typoLink('text',$linkconf);
-				// print fake link and 
+				// print fake link and
 				$content = '
 					<span style="display:none;"><a href="'.$filePath.'" rel="lightbox" id="kiwi_popup_element">image #1</a></span>
 					<script type="text/javascript">
@@ -102,10 +103,12 @@ class tx_kiwipopup_pi1 extends tslib_pibase {
 						showLightbox(elem);
 					</script>';
 				$sessionVars['shown'] = $GLOBALS['TSFE']->id;
-				
-				$GLOBALS['TSFE']->fe_user->setKey('ses', 'tx_kiwipopup', $sessionVars);
-				$GLOBALS['TSFE']->storeSessionData();
-				
+
+				if ($this->ffdata['sessionStorage']) {
+					$GLOBALS['TSFE']->fe_user->setKey('ses', 'tx_kiwipopup', $sessionVars);
+					$GLOBALS['TSFE']->storeSessionData();
+				}
+
 			}
 			return $this->pi_wrapInBaseClass($content);
 		}
